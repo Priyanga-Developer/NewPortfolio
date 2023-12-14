d1adbf8d2abfa409735834de74663c047376bc9a
Priyanga <uthirapriyanga@gmail.com>
2023-12-14 12:50:14 +0530

    darkmode added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,8 +4,9 @@ const Footer = () => {
   return (
     <footer
         className=' relative  w-full  text-center 
-          px-16 py-3 overflow-hidden'>
-          <p className=' text-base'><span className=' font-semibold'>&#169; Priyanga.All rights reserved.</span>Built with React,Tailwind CSS,Framer Motion,Vercel Hoisting </p>  
+          px-16 pb-10 overflow-hidden'>
+          <p className=' text-base font-semibold'>&#169; Coded By Priyanga.</p>
+          <p className=' text-base '> Built with React,Tailwind CSS,Framer Motion,Vercel Hoisting and ☕. </p>  
     </footer>
 
   )
