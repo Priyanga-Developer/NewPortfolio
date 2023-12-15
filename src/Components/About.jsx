5fa28bf2e0b36230b10603e57a627d2104334785
Priyanga <uthirapriyanga@gmail.com>
2023-12-15 22:53:22 +0530

    icon added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -22,10 +22,12 @@ const About = () => {
         src={profile}
         className='mb-10 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[300px] xl:h-[300px] mt-40 md:mt-0 ' 
         alt="myprofile" />
-        <div className='space-y-10 px-0 md:px-10'>
+        <div className='space-y-5 px-0 md:px-10'>
             <h4 className='text-2xl font-semibold text-center md:text-left '>Here is my SnapShot of Me!😉</h4>
             <p className='text-base text-center md:text-left'>
-            A passionate Front-end React Developer based in Bangalore,India  Hello,I am Priyanga,and currently ,am learning a web development course.Throughout this comprehensive program,I have delved into essential technologies like HTML,CSS,Javascript and React.
+             With a successful track record in talent acquisition,I begun an exiciting journey to pivot my career to frond-end development. 
+            Leveraging my expertise in problem solving and analytical skills cultivated in recruitment field,I'm actively honing my technical skills and applying them to front-end Development Projects.
+            Committed to continuous learning, I've completed web develpoment course, ensuring a seamless transition.<br/>Scroll down to know more about my skills and projects.👩🏻‍💻
             </p>
         </div>
     </motion.div>
